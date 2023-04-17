19e050dec4768da035475a92e68203a5ea1f20ec
Marcus <s2100949@edu.bc.fi>
2023-04-17 10:10:24 +0300

    Made tehtävä 7

---
+++ Väritetyt tavarat/Väritetyt tavarat/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+class Tavara
+{
+    public double Paino { get; }
+    public double Tilavuus { get; }
+
+    public Tavara(double paino, double tilavuus)
+    {
+        Paino = paino;
+        Tilavuus = tilavuus;
+    }
+
+    public override string ToString()
+    {
+        return this.GetType().Name;
+    }
+}
+
+class Nuoli : Tavara
+{
+    public Nuoli() : base(0.1, 0.05) { }
+
+    public override string ToString()
+    {
+        return "Nuoli";
+    }
+}
+
+class Jousi : Tavara
+{
+    public Jousi() : base(1, 4) { }
+
+    public override string ToString()
+    {
+        return "Jousi";
+    }
+}
+
+class Köysi : Tavara
+{
+    public Köysi() : base(1, 1.5) { }
+
+    public override string ToString()
+    {
+        return "Köysi";
+    }
+}
+
+class Vesi : Tavara
+{
+    public Vesi() : base(2, 2) { }
+
+    public override string ToString()
+    {
+        return "Vesi";
+    }
+}
+
+class RuokaAnnos : Tavara
+{
+    public RuokaAnnos() : base(1, 0.5) { }
+
+    public override string ToString()
+    {
+        return "Ruokaa";
+    }
+}
+
+class Miekka : Tavara
+{
+    public Miekka() : base(5, 3) { }
+
+    public override string ToString()
+    {
+        return "Miekka";
+    }
+}
+
+class VaritettyTavara<T> where T : Tavara
+{
+    public T Tavara { get; }
+    public ConsoleColor Vari { get; }
+
+    public VaritettyTavara(T tavara, ConsoleColor vari)
+    {
+        Tavara = tavara;
+        Vari = vari;
+    }
+
+    public void NaytaTavara()
+    {
+        Console.ForegroundColor = Vari;
+        Console.WriteLine(Tavara.ToString());
+        Console.ResetColor();
+    }
+}
+
+class Ohjelma
+{
+    static void Main(string[] args)
+    {
+        VaritettyTavara<Nuoli> varitettyNuoli = new VaritettyTavara<Nuoli>(new Nuoli(), ConsoleColor.Red);
+        varitettyNuoli.NaytaTavara();
+
+        VaritettyTavara<Miekka> varitettymiekka = new VaritettyTavara<Miekka>(new Miekka(), ConsoleColor.Blue);
+        varitettymiekka.NaytaTavara();
+
+        VaritettyTavara<Vesi> varitettymuki = new VaritettyTavara<Vesi>(new Vesi(), ConsoleColor.DarkCyan);
+        varitettymuki.NaytaTavara();
+    }
+}
---
+++ test/test/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+using Raylib_cs;
+
+namespace SpaceInvaders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int screenWidth = 800;
+            const int screenHeight = 1000;
+
+            // Initialize window
+            Raylib.InitWindow(screenWidth, screenHeight, "Space Invaders");
+
+            // Initialize player
+            Player player = new Player(new Vector2(screenWidth / 2, screenHeight - 50), 10, 100);
+
+            // Initialize enemies
+            List<Enemy> enemies = new List<Enemy>();
+            for (int i = 0; i < 5; i++)
+            {
+                Vector2 position = new Vector2(100 + i * 100, 100);
+                Enemy enemy = new Enemy(position, 4, "enemy.png");
+                enemies.Add(enemy);
+            }
+
+            // Game loop
+            while (!Raylib.WindowShouldClose())
+            {
+                // Update entities
+                player.Update(enemies);
+                foreach (Enemy enemy in enemies)
+                {
+                    enemy.Update();
+                }
+
+                // Draw entities
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Raylib_cs.Color.RAYWHITE);
+                player.Draw();
+                foreach (Enemy enemy in enemies)
+                {
+                    enemy.Draw();
+                }
+                Raylib.EndDrawing();
+            }
+
+            // Close window
+            Raylib.CloseWindow();
+        }
+
+        // Player class
+        public class Player
+        {
+            // Fields
+            public Vector2 Position { get; private set; }
+            public float Speed { get; private set; }
+            public int Health { get; private set; }
+            private List<Bullet> bullets = new List<Bullet>();
+            private Texture2D texture;
+
+            // Constructor
+            public Player(Vector2 position, float speed, int health)
+            {
+                Position = position;
+                Speed = speed / 30;
+                Health = health;
+                texture = Raylib.LoadTexture("player.png");
+            }
+
+            // Update method
+            public void Update(List<Enemy> enemies)
+            {
+                // Move player left and right
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT) && Position.X < Raylib.GetScreenWidth() - 20)
+                {
+                    Position.X += Speed;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT) && Position.X > 20)
+                {
+                    Position.X -= Speed;
+                }
+
+                // Fire bullets
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+                {
+                    Bullet bullet = new Bullet(new Vector2(Position.X, Position.Y - 20), new Vector2(0, -10));
+                    bullets.Add(bullet);
+                }
+
+                // Update bullets
+                for (int i = bullets.Count - 1; i >= 0; i--)
+                {
+                    Bullet bullet = bullets[i];
+                    bullet.Update();
+
+                    bool hitEnemy = false;
+                    // Check for collisions with enemies
+                    for (int j = enemies.Count - 1; j >= 0; j--)
+                    {
+                        Enemy enemy = enemies[j];
+                        if (Raylib.CheckCollisionCircles(bullet.Position, 5, enemy.Position, 20))
+                        {
+                            // Reduce enemy health
+                            enemy.Health -= 10;
+
+                            bullets.RemoveAt(i);
+                            hitEnemy = true;
+
+                            if (enemy.Health <= 0)
+                            {
+                                enemies.RemoveAt(j);
+                            }
+
+                            break;
+                        }
+                    }
+
+                    if (!hitEnemy && bullet.Position.Y < 0)
+                    {
+                        bullets.RemoveAt(i);
+                    }
+                }
+                // Draw method
+                public void Draw()
+                {
+                    Raylib.DrawRectangle((int)Position.X - 20, (int)Position.Y - 20, 40, 40, Color.RED);
+                    Raylib.DrawTexture(texture, (int)Position.X - texture.width / 2, (int)Position.Y - texture.height / 2, Color.WHITE);
+                }
+                // Draw bullets
+                foreach (Bullet bullet in bullets)
+                {
+                    bullet.Draw();
+                }
+            }
+
+            
+        }
+    }
+
+    // Enemy class
+    public class Enemy
+    {
+        // Fields
+        public Vector2 Position { get; private set; }
+        public float Speed { get; private set; }
+        public int Health { get; set; }
+        private Texture2D texture;
+
+        // Constructor
+        public Enemy(Vector2 position, float speed, string texturePath)
+        {
+            Position = position;
+            Speed = speed / 30;
+            Health = 20;
+            texture = Raylib.LoadTexture(texturePath);
+        }
+
+        // Update method
+        public void Update()
+        {
+            Position.Y += Speed;
+        }
+
+        // Draw method
+        public void Draw()
+        {
+            Raylib.DrawTexture(texture, (int)Position.X - texture.width / 2, (int)Position.Y - texture.height / 2, Color.WHITE);
+        }
+    }
+
+    // Bullet class
+    public class Bullet
+    {
+        // Fields
+        public Vector2 Position { get; private set; }
+        public Vector2 Velocity { get; private set; }
+
+        // Constructor
+        public Bullet(Vector2 position, Vector2 velocity)
+        {
+            Position = position;
+            Velocity = velocity;
+        }
+
+        // Update method
+        public void Update()
+        {
+            Position += Velocity;
+        }
+
+        // Draw method
+        public void Draw()
+        {
+            Raylib.DrawCircle((int)Position.X, (int)Position.Y, 5, Color.BLACK);
+        }
+    }
+}
