c9c359619b66b7f1bb9eadf968e6c483a6b138d4
Marcus <s2100949@edu.bc.fi>
2023-05-08 10:21:23 +0300

    modified sSeikkailijanreppu

--- Seikkailijanreppu/Seikkailijanreppu/Program.cs
+++ Seikkailijanreppu/Seikkailijanreppu/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-class Tavara
+abstract class Tavara
 {
     public double Paino { get; }
     public double Tilavuus { get; }
@@ -10,106 +10,86 @@ class Tavara
         Paino = paino;
         Tilavuus = tilavuus;
     }
-
-    public override string ToString()
-    {
-        return this.GetType().Name;
-    }
+    public abstract string Nimi { get; }
 }
 
 class Nuoli : Tavara
 {
     public Nuoli() : base(0.1, 0.05) { }
 
-    public override string ToString()
-    {
-        return "Nuoli";
-    }
+    public override string Nimi => "Nuoli";
 }
 
 class Jousi : Tavara
 {
     public Jousi() : base(1, 4) { }
 
-    public override string ToString()
-    {
-        return "Jousi";
-    }
+    public override string Nimi => "Jousi";
+
 }
 
 class Köysi : Tavara
 {
     public Köysi() : base(1, 1.5) { }
 
-    public override string ToString()
-    {
-        return "Köysi";
-    }
+    public override string Nimi => "Köysi";
 }
 
 class Vesi : Tavara
 {
     public Vesi() : base(2, 2) { }
 
-    public override string ToString()
-    {
-        return "Vesi";
-    }
+    public override string Nimi => "Vesi";
 }
 
 class RuokaAnnos : Tavara
 {
     public RuokaAnnos() : base(1, 0.5) { }
 
-    public override string ToString()
-    {
-        return "Ruokaa";
-    }
+    public override string Nimi => "Ruokaa";
 }
 
 class Miekka : Tavara
 {
     public Miekka() : base(5, 3) { }
 
-    public override string ToString()
-    {
-        return "Miekka";
-    }
+    public override string Nimi => "Miekka";
 }
 
 class Reppu
 {
-    public Tavara[] tavarat;
-    private int maksimiTavaroidenMaara;
-    private double maksimiKantoPaino;
-    private double maksimiTilavuus;
-    public int tavaroidenMaara = 0;
-    private double tavaroidenPaino = 0;
-    private double tavaroidenTilavuus = 0;
+    public Tavara[] Tavarat { get; }
+    public int MaksimiTavaroidenMaara { get; }
+    public double MaksimiKantoPaino { get; }
+    public double MaksimiTilavuus { get; }
+    public int TavaroidenMaara { get; private set; }
+    public double TavaroidenPaino { get; private set; }
+    public double TavaroidenTilavuus { get; private set; }
+
 
     public Reppu(int maksimiTavaroidenMaara, double maksimiKantoPaino, double maksimiTilavuus)
     {
-        this.maksimiTavaroidenMaara = maksimiTavaroidenMaara;
-        this.maksimiKantoPaino = maksimiKantoPaino;
-        this.maksimiTilavuus = maksimiTilavuus;
+        MaksimiTavaroidenMaara = maksimiTavaroidenMaara;
+        MaksimiKantoPaino = maksimiKantoPaino;
+        MaksimiTilavuus = maksimiTilavuus;
 
-        tavarat = new Tavara[maksimiTavaroidenMaara];
+        Tavarat = new Tavara[maksimiTavaroidenMaara];
     }
 
     public bool Lisää(Tavara tavara)
     {
 
-        if (tavaroidenMaara >= maksimiTavaroidenMaara ||
-            tavaroidenPaino + tavara.Paino > maksimiKantoPaino ||
-            tavaroidenTilavuus + tavara.Tilavuus > maksimiTilavuus)
+        if (TavaroidenMaara >= MaksimiTavaroidenMaara ||
+            TavaroidenPaino + tavara.Paino > MaksimiKantoPaino ||
+            TavaroidenTilavuus + tavara.Tilavuus > MaksimiTilavuus)
         {
             return false;
         }
 
-        tavarat[tavaroidenMaara] = tavara;
-        tavaroidenMaara++;
-        tavaroidenPaino += tavara.Paino;
-        tavaroidenTilavuus += tavara.Tilavuus;
+        Tavarat[TavaroidenMaara] = tavara;
+        TavaroidenMaara++;
+        TavaroidenPaino += tavara.Paino;
+        TavaroidenTilavuus += tavara.Tilavuus;
 
         return true;
     }
@@ -117,10 +97,10 @@ class Reppu
     public override string ToString()
     {
         string sisalto = "Reppussa on seuraavat tavarat: ";
-        for (int i = 0; i < tavaroidenMaara; i++)
+        for (int i = 0; i < TavaroidenMaara; i++)
         {
-            sisalto += tavarat[i].ToString();
-            if (i < tavaroidenMaara - 1)
+            sisalto += Tavarat[i].ToString();
+            if (i < TavaroidenMaara - 1)
             {
                 sisalto += ", ";
             }
@@ -132,12 +112,15 @@ class Reppu
     {
         Reppu reppu = new Reppu(10, 30, 20);
 
+        Console.WriteLine("Tervetuloa reppupeliin! Lisää tavaroita reppuun valitsemalla numero:");
+        Console.WriteLine("");
 
         for (int i = 0; i < 10; i++)
         {
             Console.WriteLine(reppu.ToString());
 
-            Console.WriteLine($"Repussa on tällä hetkllä {reppu.tavaroidenMaara}/10 tavaraa, {reppu.tavaroidenPaino}/30 painoa ja {reppu.tavaroidenTilavuus}/20 tilavuus.");
+            Console.WriteLine($"Repussa on tällä hetkllä {reppu.TavaroidenMaara}/10 tavaraa, {reppu.TavaroidenPaino}/30 painoa ja {reppu.TavaroidenTilavuus}/20 tilavuus.");
+            Console.WriteLine("");
 
             Console.WriteLine("Mitä haluat lisätä?");
             Console.WriteLine("1 - Nuoli");
@@ -153,24 +136,37 @@ class Reppu
             {
                 case "1":
                     reppu.Lisää(new Nuoli());
+                    Console.WriteLine("Nuoli lisätty reppuun.");
+                    Console.WriteLine("");
                     break;
                 case "2":
                     reppu.Lisää(new Jousi());
+                    Console.WriteLine("Jousi lisätty reppuun.");
+                    Console.WriteLine("");
                     break;
                 case "3":
                     reppu.Lisää(new Köysi());
+                    Console.WriteLine("Köysi lisätty reppuun.");
+                    Console.WriteLine("");
                     break;
                 case "4":
                     reppu.Lisää(new Vesi());
+                    Console.WriteLine("Vesi lisätty reppuun.");
+                    Console.WriteLine("");
                     break;
                 case "5":
                     reppu.Lisää(new RuokaAnnos());
+                    Console.WriteLine("Ruokaa lisätty reppuun.");
+                    Console.WriteLine("");
                     break;
                 case "6":
                     reppu.Lisää(new Miekka());
+                    Console.WriteLine("Miekka lisätty reppuun.");
+                    Console.WriteLine("");
                     break;
                 default:
                     Console.WriteLine("Virheellinen valinta!");
+                    Console.WriteLine("");
                     break;
             }
         }
